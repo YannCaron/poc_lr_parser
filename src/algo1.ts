3007dff325d9076accf7fea8df32263d8fca7c0b
Yann Caron <cyann74@gmail.com>
2024-01-30 18:20:40 +0100

    move parser main function into algorithm

--- src/algo1.ts
+++ src/algo1.ts
@@ -2,23 +2,30 @@ import Context from "./Context"
 
 // N = 2
 function parseN(ctx: Context): boolean {
-  if (ctx.canReduce('2')) {
-    ctx.reduce('N')
-    return true
-  } else {
-    return ctx.shift()
-  }
+    if (ctx.canReduce('2')) {
+        ctx.reduce('N')
+        return true
+    } else {
+        return ctx.shift()
+    }
 }
 
 // A = A '+' A | N
-export default function parseA(ctx: Context): boolean {
-  if (ctx.canReduce('A', '+', 'N')) {
-    ctx.reduce('A')
-    return parseA(ctx)
-  } else if (ctx.canReduce('N')) {
-    ctx.reduce('A')
-    return true
-  } else {
-    return parseN(ctx)
-  }
+function parseA(ctx: Context): boolean {
+    if (ctx.canReduce('A', '+', 'N')) {
+        ctx.reduce('A')
+        return parseA(ctx)
+    } else if (ctx.canReduce('N')) {
+        ctx.reduce('A')
+        return true
+    } else {
+        return parseN(ctx)
+    }
+}
+
+export default function parseLR(src: string) {
+    const ctx = new Context(src)
+
+    parseA(ctx)
+
 }
